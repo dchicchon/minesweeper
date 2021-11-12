adba3f5d28edbfabbf581ed7ee7cc9f2216f2aa7
dchicchon <danielchicchon@gmail.com>
2021-11-12 09:00:02 -0500

    fix click through by adding e.stopPropagation

--- components/Cube.js
+++ components/Cube.js
@@ -1,56 +1,113 @@
-import React, { useState, useRef } from "react";
-import { OrbitControls, Plane } from "@react-three/drei";
-// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { Canvas, useFrame, useThree, extend } from "@react-three/fiber";
+import React, { useState } from "react";
+import { OrbitControls, Plane, useCursor } from "@react-three/drei";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import styles from "../styles/game.module.css";
 import { DoubleSide } from "three";
-// extend({ OrbitControls })
-
-// const Plane = () => {
-//   return (
-//     <mesh>
-//       <planeBufferGeometry args={[1, 1, 1]} />
-//       <meshLambertMaterial color="orange" />
-//     </mesh>
-//   );
-// };
 
 const Cell = (props) => {
   const [hovered, hover] = useState(false);
+  const [checked, setChecked] = useState(false);
+
+  useCursor(hovered);
+
+  const colorStyle = () => {
+    if (hovered) return "hotpink";
+    if (checked) return "lightblue";
+    return "orange";
+  };
   return (
     <Plane
-      onPointerEnter={() => hover(true)}
-      onPointerLeave={() => hover(false)}
+      scale={1}
+      onClick={(e) => {
+        e.stopPropagation();
+        setChecked(!checked);
+      }}
+      onPointerEnter={(e) => {
+        e.stopPropagation();
+        hover(true);
+      }}
+      onPointerLeave={(e) => {
+        e.stopPropagation();
+        hover(false);
+      }}
       position={props.position}
       rotation={props.rotation}
     >
-      <meshPhongMaterial
-        side={DoubleSide}
-        color={hovered ? "hotpink" : "orange"}
-      />
+      <meshPhongMaterial side={DoubleSide} color={colorStyle()} />
     </Plane>
   );
 };
-
-const CubeFace = () => {};
-
 // maybe cube should be a bunch of planes made togther?
 const Cube = () => {
-  useFrame((state, delta) => {
-    // ref.current.rotation.x += 0.01
-    // ref.current.rotation.y += 0.01
-  });
+  useFrame((state, delta) => {});
 
-  //   Make everything centered!
-  //   Position (x, y, z)
   return (
     <>
-      <Cell position={[0, 0, -0.5]} rotation={[0, 0, 0]} />
-      <Cell position={[0, 0, 0.5]} rotation={[0, 0, 0]} />
-      <Cell position={[-0.5, 0, 0]} rotation={[0, Math.PI / 2, 0]} />
-      <Cell position={[0.5, 0, 0]} rotation={[0, Math.PI / 2, 0]} />
-      <Cell position={[0, -0.5, 0]} rotation={[Math.PI / 2, 0, 0]} />
-      <Cell position={[0, 0.5, 0]} rotation={[Math.PI / 2, 0, 0]} />
+      {/* Back Face */}
+      <Cell position={[-1, 1, -1.5]} rotation={[0, 0, 0]} />
+      <Cell position={[-1, 0, -1.5]} rotation={[0, 0, 0]} />
+      <Cell position={[-1, -1, -1.5]} rotation={[0, 0, 0]} />
+      <Cell position={[0, 1, -1.5]} rotation={[0, 0, 0]} />
+      <Cell position={[0, 0, -1.5]} rotation={[0, 0, 0]} />
+      <Cell position={[0, -1, -1.5]} rotation={[0, 0, 0]} />
+      <Cell position={[1, 1, -1.5]} rotation={[0, 0, 0]} />
+      <Cell position={[1, 0, -1.5]} rotation={[0, 0, 0]} />
+      <Cell position={[1, -1, -1.5]} rotation={[0, 0, 0]} />
+
+      {/* Front Face */}
+      <Cell position={[-1, 1, 1.5]} rotation={[0, 0, 0]} />
+      <Cell position={[-1, 0, 1.5]} rotation={[0, 0, 0]} />
+      <Cell position={[-1, -1, 1.5]} rotation={[0, 0, 0]} />
+      <Cell position={[0, 1, 1.5]} rotation={[0, 0, 0]} />
+      <Cell position={[0, 0, 1.5]} rotation={[0, 0, 0]} />
+      <Cell position={[0, -1, 1.5]} rotation={[0, 0, 0]} />
+      <Cell position={[1, 1, 1.5]} rotation={[0, 0, 0]} />
+      <Cell position={[1, 0, 1.5]} rotation={[0, 0, 0]} />
+      <Cell position={[1, -1, 1.5]} rotation={[0, 0, 0]} />
+
+      {/* Left Face */}
+      <Cell position={[-1.5, 1, 1]} rotation={[0, Math.PI / 2, 0]} />
+      <Cell position={[-1.5, 0, 1]} rotation={[0, Math.PI / 2, 0]} />
+      <Cell position={[-1.5, -1, 1]} rotation={[0, Math.PI / 2, 0]} />
+      <Cell position={[-1.5, 1, 0]} rotation={[0, Math.PI / 2, 0]} />
+      <Cell position={[-1.5, 0, 0]} rotation={[0, Math.PI / 2, 0]} />
+      <Cell position={[-1.5, -1, 0]} rotation={[0, Math.PI / 2, 0]} />
+      <Cell position={[-1.5, 1, -1]} rotation={[0, Math.PI / 2, 0]} />
+      <Cell position={[-1.5, 0, -1]} rotation={[0, Math.PI / 2, 0]} />
+      <Cell position={[-1.5, -1, -1]} rotation={[0, Math.PI / 2, 0]} />
+
+      {/* Right Face */}
+      <Cell position={[1.5, 1, 1]} rotation={[0, Math.PI / 2, 0]} />
+      <Cell position={[1.5, 0, 1]} rotation={[0, Math.PI / 2, 0]} />
+      <Cell position={[1.5, -1, 1]} rotation={[0, Math.PI / 2, 0]} />
+      <Cell position={[1.5, 1, 0]} rotation={[0, Math.PI / 2, 0]} />
+      <Cell position={[1.5, 0, 0]} rotation={[0, Math.PI / 2, 0]} />
+      <Cell position={[1.5, -1, 0]} rotation={[0, Math.PI / 2, 0]} />
+      <Cell position={[1.5, 1, -1]} rotation={[0, Math.PI / 2, 0]} />
+      <Cell position={[1.5, 0, -1]} rotation={[0, Math.PI / 2, 0]} />
+      <Cell position={[1.5, -1, -1]} rotation={[0, Math.PI / 2, 0]} />
+
+      {/* Bottom Face */}
+      <Cell position={[1, -1.5, 1]} rotation={[Math.PI / 2, 0, 0]} />
+      <Cell position={[0, -1.5, 1]} rotation={[Math.PI / 2, 0, 0]} />
+      <Cell position={[-1, -1.5, 1]} rotation={[Math.PI / 2, 0, 0]} />
+      <Cell position={[1, -1.5, 0]} rotation={[Math.PI / 2, 0, 0]} />
+      <Cell position={[0, -1.5, 0]} rotation={[Math.PI / 2, 0, 0]} />
+      <Cell position={[-1, -1.5, 0]} rotation={[Math.PI / 2, 0, 0]} />
+      <Cell position={[1, -1.5, -1]} rotation={[Math.PI / 2, 0, 0]} />
+      <Cell position={[0, -1.5, -1]} rotation={[Math.PI / 2, 0, 0]} />
+      <Cell position={[-1, -1.5, -1]} rotation={[Math.PI / 2, 0, 0]} />
+
+      {/* Top */}
+      <Cell position={[1, 1.5, 1]} rotation={[Math.PI / 2, 0, 0]} />
+      <Cell position={[0, 1.5, 1]} rotation={[Math.PI / 2, 0, 0]} />
+      <Cell position={[-1, 1.5, 1]} rotation={[Math.PI / 2, 0, 0]} />
+      <Cell position={[1, 1.5, 0]} rotation={[Math.PI / 2, 0, 0]} />
+      <Cell position={[0, 1.5, 0]} rotation={[Math.PI / 2, 0, 0]} />
+      <Cell position={[-1, 1.5, 0]} rotation={[Math.PI / 2, 0, 0]} />
+      <Cell position={[1, 1.5, -1]} rotation={[Math.PI / 2, 0, 0]} />
+      <Cell position={[0, 1.5, -1]} rotation={[Math.PI / 2, 0, 0]} />
+      <Cell position={[-1, 1.5, -1]} rotation={[Math.PI / 2, 0, 0]} />
     </>
   );
 };
@@ -58,7 +115,7 @@ const Cube = () => {
 const SceneItems = () => {
   return (
     <>
-      <OrbitControls minDistance={2} maxDistance={2.75} />
+      <OrbitControls minDistance={5} maxDistance={7} />
       <ambientLight intensity={0.5} />
       <spotLight position={[10, 15, 10]} angle={0.3} />
       <Cube position={[1, 1, 1]} />
