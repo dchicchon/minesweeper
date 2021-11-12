0740ec0051515ad70068b91032fe9c355ec96db1
dchicchon <danielchicchon@gmail.com>
2021-11-11 22:06:24 -0500

    add planes and remove Cube from Scene Items. Added rotation and position to planes as well and create Component Cell. Might add a component called CubeFace later

--- components/Cube.js
+++ components/Cube.js
@@ -1,73 +1,79 @@
-import React, { useState, useRef } from 'react'
-import { OrbitControls } from '@react-three/drei'
+import React, { useState, useRef } from "react";
+import { OrbitControls, Plane } from "@react-three/drei";
 // import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { Canvas, useFrame, useThree, extend } from '@react-three/fiber'
-import styles from '../styles/game.module.css'
+import { Canvas, useFrame, useThree, extend } from "@react-three/fiber";
+import styles from "../styles/game.module.css";
+import { DoubleSide } from "three";
 // extend({ OrbitControls })
 
-const Plane = () => {
-    return (
-        <mesh>
-            <planeBufferGeometry />
-            <meshLambertMaterial color='orange' />
-        </mesh>
-    )
-}
+// const Plane = () => {
+//   return (
+//     <mesh>
+//       <planeBufferGeometry args={[1, 1, 1]} />
+//       <meshLambertMaterial color="orange" />
+//     </mesh>
+//   );
+// };
+
+const Cell = (props) => {
+  const [hovered, hover] = useState(false);
+  return (
+    <Plane
+      onPointerEnter={() => hover(true)}
+      onPointerLeave={() => hover(false)}
+      position={props.position}
+      rotation={props.rotation}
+    >
+      <meshPhongMaterial
+        side={DoubleSide}
+        color={hovered ? "hotpink" : "orange"}
+      />
+    </Plane>
+  );
+};
+
+const CubeFace = () => {};
 
 // maybe cube should be a bunch of planes made togther?
 const Cube = () => {
-    const ref = useRef();
-    const [hovered, hover] = useState(false);
-    useFrame((state, delta) => {
-        // ref.current.rotation.x += 0.01
-        // ref.current.rotation.y += 0.01
-    })
+  useFrame((state, delta) => {
+    // ref.current.rotation.x += 0.01
+    // ref.current.rotation.y += 0.01
+  });
 
-    // return (
-    //     <>
-    //         <Plane position={[100,0,0]} />
-    //         <Plane position={[200,25,25]} />
-    //         <Plane position={[300,50,50]} />
-    //         <Plane position={[400,75 ,75]} />
-    //     </>
-    // )
-
-    return (
-        // we can try making a bunch of planes together here which are the spots for mines
-        <mesh
-            ref={ref}
-            scale={1}
-        >
-            <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color={'orange'} />
-        </mesh>
-    )
-}
-
+  //   Make everything centered!
+  //   Position (x, y, z)
+  return (
+    <>
+      <Cell position={[0, 0, -0.5]} rotation={[0, 0, 0]} />
+      <Cell position={[0, 0, 0.5]} rotation={[0, 0, 0]} />
+      <Cell position={[-0.5, 0, 0]} rotation={[0, Math.PI / 2, 0]} />
+      <Cell position={[0.5, 0, 0]} rotation={[0, Math.PI / 2, 0]} />
+      <Cell position={[0, -0.5, 0]} rotation={[Math.PI / 2, 0, 0]} />
+      <Cell position={[0, 0.5, 0]} rotation={[Math.PI / 2, 0, 0]} />
+    </>
+  );
+};
 
 const SceneItems = () => {
-    return (
-        <>
-            <OrbitControls
-                minDistance={1.75}
-                maxDistance={2.75}
-            />
-            <ambientLight intensity={0.5} />
-            <spotLight position={[10, 15, 10]} angle={0.3} />
-            <Cube position={[1, 1, 1]} />
-        </>
-    )
-}
-
+  return (
+    <>
+      <OrbitControls minDistance={2} maxDistance={2.75} />
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 15, 10]} angle={0.3} />
+      <Cube position={[1, 1, 1]} />
+    </>
+  );
+};
 
 const CompleteScene = () => {
-    return (
-        <div id={styles.scene}>
-            <Canvas>
-                <SceneItems />
-            </Canvas>
-        </div>
-    )
-}
+  return (
+    <div id={styles.scene}>
+      <Canvas>
+        <SceneItems />
+      </Canvas>
+    </div>
+  );
+};
 
-export default CompleteScene
+export default CompleteScene;
